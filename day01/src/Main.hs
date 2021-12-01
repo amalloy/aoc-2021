16b1b62298466a2191a9d954e6751a4f3454f0f4
Alan Malloy <alan@malloys.org>
2021-11-30 22:03:19 -0800

    Day 1

--- day01/src/Main.hs
+++ day01/src/Main.hs
@@ -1,20 +1,37 @@
 {-# LANGUAGE ApplicativeDo #-}
 {-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE DeriveFunctor #-}
+{-# LANGUAGE DeriveFoldable #-}
 
 module Main where
 
 import Control.Arrow ((&&&))
 
-type Input = [String]
+type Input = [Int]
 
-part1 :: Input -> ()
-part1 = const ()
+-- >>> zip [] undefined
+-- parse error on input ‘::’
+-- []
 
-part2 :: Input -> ()
-part2 = const ()
+-- prop> (\(xs :: [Int]) -> null xs || part1 xs >= 0)
+-- +++ OK, passed 100 tests.
+
+numIncreasingPairwise :: (Num a, Ord a) => [a] -> Int
+numIncreasingPairwise xs = length . filter (< 0) $ zipWith (-) xs (tail xs)
+
+part1 :: Input -> Int
+part1 = numIncreasingPairwise
+
+data Three a = Three a a a deriving (Foldable, Show)
+
+threeWindows :: [a] -> [Three a]
+threeWindows xs@(_:ys@(_:zs)) = zipWith3 Three xs ys zs
+
+part2 :: Input -> Int
+part2 = numIncreasingPairwise . map sum . threeWindows
 
 prepare :: String -> Input
-prepare = lines
+prepare = map read . lines
 
 main :: IO ()
 main = readFile "input.txt" >>= print . (part1 &&& part2) . prepare
