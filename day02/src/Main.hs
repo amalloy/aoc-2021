6768cbc4736ead19a32cf4ae8136660716dc9768
Alan Malloy <alan@malloys.org>
2021-12-02 00:19:42 -0800

    Day 2, part 2

--- day02/src/Main.hs
+++ day02/src/Main.hs
@@ -16,12 +16,20 @@ moveBy (Position d x) (Motion m n) = case m of
     Down -> v (+)
     where v op = Position (d `op` n) x
 
-
 part1 :: Input -> Int
 part1 = ((*) <$> depth <*> horiz) . foldl' moveBy (Position 0 0)
 
-part2 :: Input -> ()
-part2 = const ()
+data Missile = Missile { x, d, aim :: Int } deriving Show
+
+angle :: Missile -> Motion -> Missile
+angle (Missile x d angle) (Motion m n) = case m of
+    Forward -> Missile (x + n) (d + (n * angle)) angle
+    Down -> v (+)
+    Up -> v (-)
+    where v op = Missile x d (angle `op` n)
+
+part2 :: Input -> Int
+part2 = ((*) <$> d <*> x) . foldl' angle (Missile 0 0 0)
 
 prepare :: String -> Input
 prepare = map (parse . words) . lines
