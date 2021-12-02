063837a832da1466405506b4a0d66430eaffaf7c
Alan Malloy <alan@malloys.org>
2021-12-02 02:16:29 -0800

    Day 2: move folding functoins into where clauses

--- day02/src/Main.hs
+++ day02/src/Main.hs
@@ -9,27 +9,23 @@ data Position = Position {depth, horiz :: Int} deriving Show
 
 type Input = [Motion]
 
-moveBy :: Position -> Motion -> Position
-moveBy (Position d x) (Motion m n) = case m of
-    Forward -> Position d (x + n)
-    Up -> v (-)
-    Down -> v (+)
-    where v op = Position (d `op` n) x
-
 part1 :: Input -> Int
 part1 = ((*) <$> depth <*> horiz) . foldl' moveBy (Position 0 0)
+  where moveBy (Position d x) (Motion m n) = case m of
+          Forward -> Position d (x + n)
+          Up -> v (-)
+          Down -> v (+)
+          where v op = Position (d `op` n) x
 
 data Missile = Missile { x, d, aim :: Int } deriving Show
 
-angle :: Missile -> Motion -> Missile
-angle (Missile x d angle) (Motion m n) = case m of
-    Forward -> Missile (x + n) (d + (n * angle)) angle
-    Down -> v (+)
-    Up -> v (-)
-    where v op = Missile x d (angle `op` n)
-
 part2 :: Input -> Int
 part2 = ((*) <$> d <*> x) . foldl' angle (Missile 0 0 0)
+  where angle (Missile x d angle) (Motion m n) = case m of
+          Forward -> Missile (x + n) (d + (n * angle)) angle
+          Down -> v (+)
+          Up -> v (-)
+          where v op = Missile x d (angle `op` n)
 
 prepare :: String -> Input
 prepare = map (parse . words) . lines
