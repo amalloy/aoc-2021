13fbd41ee9901ed26d177bab171cba2c7a8fd611
Alan Malloy <alan@malloys.org>
2021-12-02 00:09:48 -0800

    Day 2, part 1

--- day02/src/Main.hs
+++ day02/src/Main.hs
@@ -1,20 +1,35 @@
-{-# LANGUAGE ApplicativeDo #-}
-{-# LANGUAGE LambdaCase #-}
-
 module Main where
 
 import Control.Arrow ((&&&))
+import Data.List (foldl')
 
-type Input = [String]
+data Direction = Up | Down | Forward deriving Show
+data Motion = Motion Direction Int deriving Show
+data Position = Position {depth, horiz :: Int} deriving Show
 
-part1 :: Input -> ()
-part1 = const ()
+type Input = [Motion]
+
+moveBy :: Position -> Motion -> Position
+moveBy (Position d x) (Motion m n) = case m of
+    Forward -> Position d (x + n)
+    Up -> v (-)
+    Down -> v (+)
+    where v op = Position (d `op` n) x
+
+
+part1 :: Input -> Int
+part1 = ((*) <$> depth <*> horiz) . foldl' moveBy (Position 0 0)
 
 part2 :: Input -> ()
 part2 = const ()
 
 prepare :: String -> Input
-prepare = lines
+prepare = map (parse . words) . lines
+  where parse [dir, n] = Motion d (read n)
+          where d = case dir of
+                      "down" -> Down
+                      "up" -> Up
+                      "forward" -> Forward
 
 main :: IO ()
 main = readFile "input.txt" >>= print . (part1 &&& part2) . prepare
