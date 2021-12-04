6547445741f20842f6b585488800e9476fc52504
Alan Malloy <alan@malloys.org>
2021-12-03 22:25:24 -0800

    Day 4, part 1

--- day04/src/Main.hs
+++ day04/src/Main.hs
@@ -2,10 +2,13 @@
 
 module Main where
 
-import Text.Regex.Applicative
+import Text.Regex.Applicative ( sym, Alternative(many), RE, (=~), psym )
 
 import Control.Arrow ((&&&))
 import Text.Regex.Applicative.Common (decimal)
+import Data.Maybe (fromJust, isNothing, catMaybes)
+import Data.Foldable (toList)
+import Data.Char (isSpace)
 
 data Five a = Five a a a a a deriving (Foldable, Functor, Show)
 instance Applicative Five where
@@ -13,9 +16,10 @@ instance Applicative Five where
   Five fa fb fc fd fe <*> Five a b c d e = Five (fa a) (fb b) (fc c) (fd d) (fe e)
 instance Traversable Five where
   sequenceA (Five fa fb fc fd fe) = Five <$> fa <*> fb <*> fc <*> fd <*> fe
-newtype Board a = Board (Five (Five a)) deriving (Show, Functor)
+newtype Board a = Board (Five (Five a)) deriving (Show, Functor, Foldable)
 
 data Input = Input [Int] [Board Int] deriving Show
+data State = State {_lastCalled :: Int, _boards :: [Board (Maybe Int)]} deriving Show
 
 type Regex a = RE Char a
 
@@ -25,29 +29,43 @@ p `sepBy` delim = (:) <$> p <*> many (delim *> p)
 numbers :: Regex [Int]
 numbers = decimal `sepBy` sym ','
 
-fiveSepBy :: Regex a -> Regex b -> Regex (Five a)
-fiveSepBy p delim = Five <$> p <*> dp <*> dp <*> dp <*> dp
-  where dp = delim *> p
+fiveTimes :: Regex a -> Regex (Five a)
+fiveTimes p = Five <$> p <*> p <*> p <*> p <*> p
 
 board :: Regex (Board Int)
-board = Board <$> (sym '\n' *> fiveSepBy row (sym '\n'))
-  where row = fiveSepBy decimal (many (sym ' '))
-
-testInput :: String 
-testInput = let b = concat . replicate 5 $ "\n1    2 4 10 20"
-            in "1,2,10" ++ "\n" ++ b ++ "\n" ++ b
+board = Board <$> fiveTimes (sym '\n' *> row)
+  where row = fiveTimes $ many (sym ' ') *> decimal
 
 input :: Regex Input
-input = Input <$> numbers <*> many (sym '\n' *> board)
+input = Input <$> numbers <*> many (sym '\n' *> board) <* many (psym isSpace)
 
-part1 :: Input -> ()
-part1 = const ()
+mark :: Eq a => a -> Board (Maybe a) -> Board (Maybe a)
+mark x = fmap (erase (Just x))
+  where erase x y | x == y = Nothing
+                  | otherwise = y
+
+winner :: Board (Maybe a) -> Bool
+winner (Board b) = any (all isNothing) b || any (all isNothing) (sequenceA b)
+
+states :: Input -> [State]
+states (Input nums boards) = go nums (fmap (fmap Just) boards)
+  where go [] _ = []
+        go (n:ns) boards = State n boards' : go ns boards'
+          where boards' = mark n <$> boards
+
+part1 :: Input -> Int
+part1 = go . states
+  where go [] = error "no winner"
+        go (State n bs : ss) = case filter winner bs of
+          [] -> go ss
+          b : _ -> n * (sum . catMaybes . toList $ b)
+
 
 part2 :: Input -> ()
 part2 = const ()
 
 prepare :: String -> Input
-prepare = undefined
+prepare = fromJust . (=~ input)
 
 main :: IO ()
 main = readFile "input.txt" >>= print . (part1 &&& part2) . prepare
