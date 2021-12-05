9c2db2cbe687f818d2777ea14d97dd30bc7e1583
Alan Malloy <alan@malloys.org>
2021-12-04 21:19:15 -0800

    Day 5: Parser

--- day05/src/Main.hs
+++ day05/src/Main.hs
@@ -1,11 +1,27 @@
-{-# LANGUAGE ApplicativeDo #-}
-{-# LANGUAGE LambdaCase #-}
-
 module Main where
 
 import Control.Arrow ((&&&))
+import qualified Data.Map as M
+import Data.Maybe (fromMaybe)
+import Text.Regex.Applicative
+import Text.Regex.Applicative.Common (decimal)
 
-type Input = [String]
+data Line = Line { _from, _to :: Coord } deriving (Show, Eq, Ord)
+type Grid a = M.Map Coord a
+type Coord = (Int, Int)
+
+type Regex a = RE Char a
+
+coord :: Regex Coord
+coord = (,) <$> decimal <* sym ',' <*> decimal
+
+line :: Regex Line
+line = Line <$> coord <* string " -> " <*> coord <* sym '\n'
+
+input :: Regex Input
+input = many line
+
+type Input = [Line]
 
 part1 :: Input -> ()
 part1 = const ()
@@ -14,7 +30,7 @@ part2 :: Input -> ()
 part2 = const ()
 
 prepare :: String -> Input
-prepare = lines
+prepare = fromMaybe [] . (=~ input)
 
 main :: IO ()
 main = readFile "input.txt" >>= print . (part1 &&& part2) . prepare
