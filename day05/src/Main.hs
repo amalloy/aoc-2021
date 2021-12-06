a2279d821fc10443e065d470b6bc096651d1f0dc
Alan Malloy <alan@malloys.org>
2021-12-05 18:30:33 -0800

    Day 5: Simplify a lot by writing a function of type Line -> [Coord]

--- day05/src/Main.hs
+++ day05/src/Main.hs
@@ -1,7 +1,6 @@
 module Main where
 
 import Control.Arrow ((&&&))
-import Data.Foldable (toList)
 import Data.List (foldl')
 import qualified Data.Map.Strict as M
 import Data.Maybe (fromMaybe)
@@ -9,9 +8,7 @@ import Text.Regex.Applicative
 import Text.Regex.Applicative.Common (decimal)
 
 data Line = Line { _from, _to :: Coord } deriving (Show, Eq, Ord)
-type Grid a = M.Map Coord a
 type Coord = (Int, Int)
-data Orientation = Vertical | Horizontal | Diagonal deriving (Show, Enum, Eq, Ord)
 
 type Regex a = RE Char a
 
@@ -26,42 +23,28 @@ input = many line
 
 type Input = [Line]
 
-orientation :: Line -> Orientation
-orientation (Line (x1, y1) (x2, y2)) | x1 == x2 = Vertical
-                                     | y1 == y2 = Horizontal
-                                     | otherwise = Diagonal
+diagonal :: Line -> Bool
+diagonal (Line (x1, y1) (x2, y2)) = abs (x1 - x2) == abs (y1 - y2)
 
-importLines :: (Line -> [Coord]) -> [Line] -> Grid [Line]
-importLines diagHandler = foldl' addToGrid M.empty
-  where addToGrid m line = case orientation line of
-          Diagonal -> foldl' addPoint m (diagHandler line)
-            where addPoint m coord = M.insertWith (<>) coord [line] m
-          Vertical -> let Line (x, y1) (_, y2) = line
-                          from = min y1 y2
-                          to = max y1 y2
-                          addPoint m y = M.insertWith (<>) (x, y) [line] m
-                      in foldl' addPoint m [from..to]
-          Horizontal -> let Line (x1, y) (x2, _) = line
-                            from = min x1 x2
-                            to = max x1 x2
-                            addPoint m x = M.insertWith (<>) (x, y) [line] m
-                        in foldl' addPoint m [from..to]
-
-countOverlaps :: Foldable f => f [a] -> Int
-countOverlaps = length . filter ((> 1) . length) . toList
-
-part1 :: Input -> Int
-part1 = countOverlaps . importLines mempty
-
-pointsOnDiagonal :: Line -> [Coord]
-pointsOnDiagonal (Line start@(x1, y1) (x2, y2)) =
-  let len = abs (x1 - x2)
+pointsOnLine :: Line -> [Coord]
+pointsOnLine (Line from@(x1, y1) (x2, y2)) =
+  let len = 1 + max (abs (x1 - x2)) (abs (y1 - y2))
       (dx, dy) = (signum (x2 - x1), signum (y2 - y1))
       move (x, y) = (x + dx, y + dy)
-  in take (len + 1) (iterate move start)
+  in take len $ iterate move from
+
+importLines :: [Line] -> M.Map Coord Int
+importLines = foldl' add M.empty . (>>= pointsOnLine)
+  where add m k = M.insertWith (+) k 1 m
+
+countOverlaps :: M.Map k Int -> Int
+countOverlaps = length . filter (> 1) . M.elems
+
+part1 :: Input -> Int
+part1 = countOverlaps . importLines . filter (not . diagonal)
 
 part2 :: Input -> Int
-part2 = countOverlaps . importLines pointsOnDiagonal
+part2 = countOverlaps . importLines
 
 prepare :: String -> Input
 prepare = fromMaybe [] . (=~ input)
