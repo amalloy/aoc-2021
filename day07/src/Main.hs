eaa2c0df9dd574bfe95a2a133960f6f4064ccb5f
Alan Malloy <alan@malloys.org>
2021-12-06 21:57:50 -0800

    Day 7, part 2

--- day07/src/Main.hs
+++ day07/src/Main.hs
@@ -17,8 +17,16 @@ part1 input = do
   med <- median input
   pure . sum . fmap (abs . (subtract med)) $ input
 
-part2 :: Input -> ()
-part2 = const ()
+cost :: Int -> Int -> Int
+cost from to = let n = (abs $ from - to)
+               in n * (n + 1) `div` 2
+
+part2 :: Input -> Int
+part2 crabs = case (S.viewl crabs, S.viewr crabs) of
+  (lo S.:< _, _ S.:> hi) -> minimum $ do
+    pos <- [lo..hi]
+    pure . sum . fmap (cost pos) $ crabs
+  _ -> 0
 
 prepare :: String -> Input
 prepare = S.fromList . sort . fromMaybe [] . (=~ input)
