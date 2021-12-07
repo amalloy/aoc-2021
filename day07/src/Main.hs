53e6e048cb54c23eaa16a374f0bbe1b33f57b7b0
Alan Malloy <alan@malloys.org>
2021-12-06 21:45:48 -0800

    Day 7, part 1

--- day07/src/Main.hs
+++ day07/src/Main.hs
@@ -1,20 +1,28 @@
-{-# LANGUAGE ApplicativeDo #-}
-{-# LANGUAGE LambdaCase #-}
-
 module Main where
 
 import Control.Arrow ((&&&))
+import Data.List (sort)
+import Data.Maybe (fromMaybe)
+import qualified Data.Sequence as S
+import Text.Regex.Applicative
+import Text.Regex.Applicative.Common (decimal)
 
-type Input = [String]
+type Input = S.Seq Int
 
-part1 :: Input -> ()
-part1 = const ()
+median :: S.Seq a -> Maybe a
+median xs = S.lookup (S.length xs `div` 2) xs
+
+part1 :: Input -> Maybe Int
+part1 input = do
+  med <- median input
+  pure . sum . fmap (abs . (subtract med)) $ input
 
 part2 :: Input -> ()
 part2 = const ()
 
 prepare :: String -> Input
-prepare = lines
+prepare = S.fromList . sort . fromMaybe [] . (=~ input)
+  where input = many (decimal <* anySym)
 
 main :: IO ()
 main = readFile "input.txt" >>= print . (part1 &&& part2) . prepare
