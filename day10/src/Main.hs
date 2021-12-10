6239324357cac54ede8dedcddaba2fb31d2a2aa6
Alan Malloy <alan@malloys.org>
2021-12-10 00:56:02 -0800

    Day 10, part 1

--- day10/src/Main.hs
+++ day10/src/Main.hs
@@ -1,14 +1,36 @@
-{-# LANGUAGE ApplicativeDo #-}
-{-# LANGUAGE LambdaCase #-}
-
 module Main where
 
 import Control.Arrow ((&&&))
+import Data.Maybe (mapMaybe)
+
+data Closer = Closer { _opener :: Char, _score :: Int } deriving Show
+data ParseResult = Correct | Corrupted Int | Incomplete [Char] deriving Show
+
+closer :: Char -> Maybe Closer
+closer = flip lookup
+  [ (')', Closer '(' 3)
+  , (']', Closer '[' 57)
+  , ('}', Closer '{' 1197)
+  , ('>', Closer '<' 25137)
+  ]
 
 type Input = [String]
 
-part1 :: Input -> ()
-part1 = const ()
+corruptedScore :: [Char] -> ParseResult
+corruptedScore = go []
+  where go [] [] = Correct
+        go s [] = Incomplete s
+        go stack (i:input) = case closer i of
+          Nothing -> go (i:stack) input
+          Just (Closer expected score) -> case stack of
+            [] -> error "closer with no matching open at all"
+            s:more | expected == s -> go more input
+                   | otherwise -> Corrupted score
+
+part1 :: Input -> Int
+part1 = sum . mapMaybe (getScore . corruptedScore)
+  where getScore (Corrupted n) = Just n
+        getScore _ = Nothing
 
 part2 :: Input -> ()
 part2 = const ()
