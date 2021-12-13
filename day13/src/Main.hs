8c98d65be618fd25297d855c8652beb81b944d95
Alan Malloy <alan@malloys.org>
2021-12-12 23:25:29 -0800

    Day 13, part 1

--- day13/src/Main.hs
+++ day13/src/Main.hs
@@ -1,20 +1,41 @@
-{-# LANGUAGE ApplicativeDo #-}
-{-# LANGUAGE LambdaCase #-}
-
 module Main where
 
 import Control.Arrow ((&&&))
+import Data.Maybe (fromMaybe)
 
-type Input = [String]
+import Text.Regex.Applicative
+import Text.Regex.Applicative.Common (decimal)
 
-part1 :: Input -> ()
-part1 = const ()
+import qualified Data.Set as S
+
+data Point = Point { _x, _y :: Int } deriving (Eq, Ord, Show)
+data Axis = X | Y
+data Fold = Axis := Int
+type Paper = S.Set Point
+
+data Input = Input Paper [Fold]
+
+fold :: Fold -> Paper -> Paper
+fold f = S.map (transform f)
+  where transform (X := i) p@(Point x y) | x < i = p
+                                         | otherwise = Point (i - (x - i)) y
+        transform (Y := i) p@(Point x y) | y < i = p
+                                         | otherwise = Point x (i - (y - i))
+
+part1 :: Input -> Int
+part1 (Input p (f:_)) = S.size (fold f p)
 
 part2 :: Input -> ()
 part2 = const ()
 
 prepare :: String -> Input
-prepare = lines
+prepare = fromMaybe (Input mempty mempty) . (=~ input)
+  where input = Input <$> dots <* sym '\n' <*> many fold
+        dots = S.fromList <$> many point
+        fold = (:=) <$> (string "fold along " *> axis) <* sym '=' <*> decimal <* sym '\n'
+        axis = Y <$ sym 'y'
+           <|> X <$ sym 'x'
+        point = Point <$> decimal <* sym ',' <*> decimal <* sym '\n'
 
 main :: IO ()
 main = readFile "input.txt" >>= print . (part1 &&& part2) . prepare
