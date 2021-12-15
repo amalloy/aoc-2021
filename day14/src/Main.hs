77b0aef3bdbdd37b969e5d1870deea4e4db2d772
Alan Malloy <alan@malloys.org>
2021-12-15 00:31:53 -0800

    Day 14, parts 1 and 2 (whoops)

--- day14/src/Main.hs
+++ day14/src/Main.hs
@@ -11,26 +11,45 @@ newtype Element = Element Char deriving (Eq, Ord, Show)
 data Pair a = Pair a a deriving (Eq, Ord, Show)
 data Rule a = Rule (Pair a) a deriving (Show)
 
-type Polymer a = M.Map (Pair a) Int
-data Input = Input (Polymer Element) (Pair Element -> Element)
+type Polymer a = M.Map (Pair a) Integer
+data Input = Input { _elements :: Polymer Element, _transition :: Pair Element -> Element ,
+                     _first :: Element}
 
-part1 :: Input -> ()
-part1 = const ()
+step :: Input -> Input
+step (Input m f i) = Input m' f i
+  where m' = M.fromListWith (+) $ do
+          (p@(Pair a b), n) <- M.assocs m
+          let between = f p
+          [(Pair a between, n), (Pair between b, n)]
 
-part2 :: Input -> ()
-part2 = const ()
+score :: Input -> Integer
+score (Input m _ i) = maximum quantities - minimum quantities
+  where quantities = M.elems (M.insertWith (+) i 1 (M.fromListWith (+) $ do
+                                                       (Pair _a b, n) <- M.assocs m
+                                                       [(b, n)]))
+
+solve :: Int -> Input -> Integer
+solve n = score . (!! n) . iterate step
+
+part1 :: Input -> Integer
+part1 = solve 10
+
+part2 :: Input -> Integer
+part2 = solve 40
+
+extraCredit = solve 200
 
 prepare :: String -> Input
-prepare = fromMaybe (Input mempty undefined) . (=~ input)
-  where input = Input <$> polymer <* string "\n\n" <*> transition
+prepare = fromMaybe (Input mempty undefined undefined) . (=~ input)
+  where input = mkInput <$> many element <* string "\n\n" <*> transition
+          where mkInput es f = Input (mkPolymer es) f (head es)
+                mkPolymer es = M.fromListWith (+) $ zipWith entry es (tail es)
+                entry a b = (Pair a b, 1)
         element = Element <$> psym isUpper
         transition = mkTransition <$> many rule
           where mkTransition rules = ((M.fromList [(p, e) | (Rule p e) <- rules]) M.!)
         rule = Rule <$> pair <* string " -> " <*> element <* sym '\n'
         pair = Pair <$> element <*> element
-        polymer = mkPolymer <$> many element
-          where mkPolymer es = M.fromListWith (+) $ zipWith entry es (tail es)
-                entry a b = (Pair a b, 1)
 
 main :: IO ()
-main = readFile "input.txt" >>= print . (part1 &&& part2) . prepare
+main = readFile "input.txt" >>= print . (part1 &&& part2 &&& extraCredit) . prepare
