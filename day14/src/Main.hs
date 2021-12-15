761bc8e2a3ffaf0de348f1866c5ea0aac83c7014
Alan Malloy <alan@malloys.org>
2021-12-14 23:33:43 -0800

    Day 14: Types and parser

--- day14/src/Main.hs
+++ day14/src/Main.hs
@@ -1,11 +1,18 @@
-{-# LANGUAGE ApplicativeDo #-}
-{-# LANGUAGE LambdaCase #-}
-
 module Main where
 
 import Control.Arrow ((&&&))
+import Data.Char (isUpper)
+import Data.Maybe (fromMaybe)
 
-type Input = [String]
+import qualified Data.Map.Strict as M
+import Text.Regex.Applicative
+
+newtype Element = Element Char deriving (Eq, Ord, Show)
+data Pair a = Pair a a deriving (Eq, Ord, Show)
+data Rule a = Rule (Pair a) a deriving (Show)
+
+type Polymer a = M.Map (Pair a) Int
+data Input = Input (Polymer Element) [Rule Element] deriving Show
 
 part1 :: Input -> ()
 part1 = const ()
@@ -14,7 +21,14 @@ part2 :: Input -> ()
 part2 = const ()
 
 prepare :: String -> Input
-prepare = lines
+prepare = fromMaybe (Input mempty mempty) . (=~ input)
+  where input = Input <$> polymer <* string "\n\n" <*> many rule
+        element = Element <$> psym isUpper
+        rule = Rule <$> pair <* string " -> " <*> element <* sym '\n'
+        pair = Pair <$> element <*> element
+        polymer = mkPolymer <$> many element
+          where mkPolymer es = M.fromListWith (+) $ zipWith entry es (tail es)
+                entry a b = (Pair a b, 1)
 
 main :: IO ()
 main = readFile "input.txt" >>= print . (part1 &&& part2) . prepare
