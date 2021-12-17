c07b9967bcd8d494458399a92f36d4ef95e13dd1
Alan Malloy <alan@malloys.org>
2021-12-16 22:42:38 -0800

    Day 15: Use a heap

--- day15/src/Main.hs
+++ day15/src/Main.hs
@@ -4,35 +4,41 @@ module Main where
 
 import Control.Arrow ((&&&))
 import Control.Monad.ST
+import Control.Monad (when)
 import Data.Foldable (for_)
 import Data.Char (digitToInt)
 import Data.Ord (comparing)
 import Data.List (minimumBy)
+
 import Data.STRef
+
+import Data.Heap (Heap, Entry(..))
+import qualified Data.Heap as H
 import Data.Array as A
-import Data.Map as M
-import Data.Set as S
+import qualified Data.Map as M
+import qualified Data.Set as S
 
 newtype Risk = Risk Int deriving (Eq, Ord, Num, Show, Bounded)
 data Coord = Coord { _x, _y :: Int } deriving (Eq, Ord, Show, Ix)
 
 lowestRisk :: Array Coord Risk -> Risk
 lowestRisk a = runST $ do
-  let (start, end) = bounds a
-  q <- newSTRef (S.fromList (indices a))
+  let b@(start, end) = bounds a
+  q <- newSTRef (H.singleton (Entry 0 start))
   dist <- newSTRef (M.singleton start 0)
   let getDist n = M.findWithDefault maxBound n <$> readSTRef dist
   let search = do
-        opens <- S.toList <$> readSTRef q
-        weighted <- traverse (\v -> do
-                                 d <- getDist v
-                                 pure (v, d))
-                    opens
-        let (u, _weight) = minimumBy (comparing snd) weighted
-        modifySTRef q $ S.delete u
-        for_ (neighbors u) $ \v -> do
-          alt <- (+ (a A.! v)) <$> getDist u
-          modifySTRef dist (M.insertWith min v alt)
+        Entry risk u <- H.minimum <$> readSTRef q
+        modifySTRef q H.deleteMin
+        tentativeDistance <- getDist u
+        when (risk == tentativeDistance) $ do
+          for_ (neighbors u) $ \v -> do
+            when (inRange b v) $ do
+              alt <- (+ (a A.! v)) <$> getDist u
+              oldDist <- getDist v
+              when (alt < oldDist) $ do
+                modifySTRef dist (M.insert v alt)
+                modifySTRef q (H.insert (Entry alt v))
         if u == end
           then getDist u
           else search
