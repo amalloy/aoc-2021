bbcc433890ae933c51927dfba95ad938df835d7a
Alan Malloy <alan@malloys.org>
2021-12-17 23:23:35 -0800

    Day 16: Packet types

--- day16/src/Main.hs
+++ day16/src/Main.hs
@@ -1,9 +1,28 @@
-{-# LANGUAGE ApplicativeDo #-}
-{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 
 module Main where
 
+import Data.List (foldl')
+
 import Control.Arrow ((&&&))
+
+import Text.Parsec
+
+data Bit = Zero | One deriving (Show)
+newtype VersionId = VersionId Int deriving (Show, Num)
+newtype PacketType = PacketType Int deriving (Show, Num)
+data LengthType = BitLength | NumPackets deriving (Show)
+newtype Literal = Literal Int deriving (Show, Num)
+data Versioned a = Versioned VersionId a deriving (Show)
+data PacketContents = LiteralPacket Literal | OperatorPacket PacketType [Packet] deriving (Show)
+newtype Packet = Packet (Versioned PacketContents) deriving (Show)
+
+type Parser a = Parsec [Bit] () a
+
+fromBinary :: [Bit] -> Int
+fromBinary = foldl' nextBit 0
+  where nextBit acc Zero = 2 * acc
+        nextBit acc One = 2 * acc + 1
 
 type Input = [String]
 
